087918049cde46ddcc4aead6c0e8593bed76805d
Janek <j.brunow@lastlevel.pl>
2023-05-05 15:31:24 +0200

    commit related with creating migrations.

--- application/__init__.py
+++ application/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from application import users
 from application import models
