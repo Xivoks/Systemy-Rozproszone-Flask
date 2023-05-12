2b64fe788173ff63b1d3ba7e2e1249521329df5e
Janek <j.brunow@lastlevel.pl>
2023-05-12 18:04:43 +0200

    commit related with update and delete users from database.

--- application/users.py
+++ application/users.py
@@ -2,6 +2,8 @@ from application import app, db
 from flask import jsonify, request
 from application.models import Users, UserSchema, user_schema
 from webargs.flaskparser import use_args
+from application.utils import validate_json_content_type
+
 
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
@@ -25,8 +27,9 @@ def get_user(user_id: int):
 
 @app.route('/api/v1/users', methods=['POST'])
 @use_args(user_schema)
+@validate_json_content_type
 def create_user(args: dict):
-    user=Users(**args)
+    user = Users(**args)
     db.session.add(user)
     db.session.commit()
     return jsonify({
@@ -36,7 +39,14 @@ def create_user(args: dict):
 
 
 @app.route('/api/v1/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id: int):
+@validate_json_content_type
+@use_args(user_schema, error_status_code=400)
+def update_user(args: dict, user_id: int):
+    user = Users.query.get_or_404(user_id, description=f'User with id {user_id} not found')
+    user.name = args['name']
+    user.age = args['age']
+    user.email = args['email']
+    db.session.commit()
     return jsonify({
         'success': True,
         'data': f'User with id {user_id} has been updated'
@@ -45,6 +55,9 @@ def update_user(user_id: int):
 
 @app.route('/api/v1/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id: int):
+    user = Users.query.get_or_404(user_id, description=f'User with id {user_id} not found')
+    db.session.delete(user)
+    db.session.commit()
     return jsonify({
         'success': True,
         'data': f'User with id {user_id} has been deleted'
